146a1995978a50e6612fdc13dc325ca925703537
Shailendra Mathur <shl.mathur@gmail.com>
2020-08-09 10:28:21 +0530

    Deleted Greetings.js

--- public/Greeting.js
+++
@@ -1,6 +0,0 @@
-class Greeting extends React.Component {
-    render() {
-        return (React.createElement('h1',null,`Hi ${this.props.person}`));
-    }
-}
-ReactDOM.render(React.createElement(Greeting,{person: 'Mathur'},null),document.getElementById('root'));
